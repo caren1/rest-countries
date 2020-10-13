6ebb1837a5a40d3838058978a1d0e4563d8fd225
caren1 <czarnocki.w@wp.pl>
2020-10-13 18:09:11 +0200

    Updates

---
+++ precache-manifest.b3b0250b6562a5d369360a9a90891f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e4b88ab96ef91f26bdb614dac1fad7",
+    "url": "/rest-countries/index.html"
+  },
+  {
+    "revision": "7307614ded888240cc9b",
+    "url": "/rest-countries/static/css/main.ae80a23d.chunk.css"
+  },
+  {
+    "revision": "b674978c563f9afbc089",
+    "url": "/rest-countries/static/js/2.99ded539.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/rest-countries/static/js/2.99ded539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7307614ded888240cc9b",
+    "url": "/rest-countries/static/js/main.3f632e6b.chunk.js"
+  },
+  {
+    "revision": "2f37a1d2d002ade89fb7",
+    "url": "/rest-countries/static/js/runtime-main.610a0620.js"
+  }
+]);
